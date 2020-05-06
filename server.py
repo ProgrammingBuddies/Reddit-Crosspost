4a411c705bdf498f5c45e97b4275d7f9bac8db11
Adenylatcyclase <Streicher1993@googlemail.com>
2020-05-06 20:21:23 +0200

    just use ids to determine which posts are new. this api ...

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import praw
 from os import getenv
 from discord_webhook import DiscordWebhook
 import time
+import base36
 
 
 class ProgrammingBuddiesBot:
@@ -12,9 +13,8 @@ class ProgrammingBuddiesBot:
                                   user_agent='ProgrammingBuddiesBot')
         self.subreddit = getenv('SUBREDDIT')
         self.webhook_url = getenv("WEBHOOK")
-        self.buffer = []
-        self.post_limit = int(getenv("POST_LIMIT", 10))
-        self.buffer_size = int(getenv("BUFFER_SIZE", 100))
+        self.last_id = -1
+        self.fetch_size = int(getenv("FETCH_SIZE", 10))
 
     def loop(self):
         '''Request the newest Posts from a subreddit and post them to a
@@ -23,33 +23,18 @@ class ProgrammingBuddiesBot:
            Will not post anything on the first run.'''
 
         sub = self.reddit.subreddit(self.subreddit)
-        for i, id in enumerate(self.buffer[::-1]):
-            try:
-                time.sleep(1)
-                post = praw.models.Submission(self.reddit, id)
-                if post is None:
-                    self.buffer = self.buffer[:-1*i]
-                    print("post was None")
-                else:
-                    print(f"get items before t3_{id}")
-                    posts = list(sub.new(limit=self.post_limit, params={"before": f't3_{id}'}))[::-1]
-                    break
-            except Exception as e:
-                print(e)
-        else:
+        posts = list(sub.new(limit=self.fetch_size))[::-1]
+        if self.last_id == -1:
             print("initial loop or unusable buffer yo")
-            posts = sub.new(limit=self.post_limit)
-            self.buffer += [post.id for post in list(posts)[::-1]]
+            self.last_id = base36.loads(posts[-1].id)
             return
 
         for post in posts:
-            self.hook(post.url)
-            self.buffer.append(f'{post.id}')
-            print(f't3_{post.id}: {post.url}')
-    
-    def clean(self):
-        if len(self.buffer) > self.buffer_size:
-            self.buffer = self.buffer[len(self.buffer) - self.buffer_size:]
+            if base36.loads(post.id) > self.last_id:
+                self.hook(post.url)
+                print(f't3_{post.id}: {post.url}')
+
+        self.last_id = base36.loads(posts[-1].id)
 
     def hook(self, msg):
         webhook = DiscordWebhook(url=self.webhook_url, content=msg)
@@ -67,5 +52,5 @@ if __name__ == "__main__":
             time.sleep(waiting_time * 60)
         except KeyboardInterrupt:
             exit()
-        except Exception as e:
-            print(f'EXCEPTION\n{e}')
+        #except Exception as e:
+        #    print(f'EXCEPTION\n{e}')
